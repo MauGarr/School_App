1cfae1d80abb96497a7db025e859098af17e9370
MauGarr <3292719381105@ingenieria.usac.edu.gt>
2024-03-24 12:10:17 -0600

    Se realizan modificaciones a la peticiones en el backend

--- backend/src/students/dto/create-student.dto.ts
+++ backend/src/students/dto/create-student.dto.ts
@@ -1,10 +1,31 @@
+import { IsString, IsNotEmpty, IsDateString } from 'class-validator';
 
-export class CreateStudentDto{
+export class CreateStudentDto {
+    @IsString()
+    @IsNotEmpty()
     nombre: string;
+
+    @IsDateString()
+    @IsNotEmpty()
     fechanacimiento: Date;
+
+    @IsString()
+    @IsNotEmpty()
     nombrepadre: string;
+
+    @IsString()
+    @IsNotEmpty()
     nombremadre: string;
+
+    @IsString()
+    @IsNotEmpty()
     grado: string;
+
+    @IsString()
+    @IsNotEmpty()
     seccion: string;
+
+    @IsDateString()
+    @IsNotEmpty()
     fechaingreso: Date;
 }
--- backend/src/students/dto/update-student.dto.ts
+++ backend/src/students/dto/update-student.dto.ts
@@ -1,10 +1,31 @@
+import { IsOptional, IsString, IsDateString } from 'class-validator';
 
-export class UpdateStudentDto{
+export class UpdateStudentDto {
+    @IsOptional()
+    @IsString()
     nombre?: string;
+
+    @IsOptional()
+    @IsDateString()
     fechanacimiento?: Date;
+
+    @IsOptional()
+    @IsString()
     nombrepadre?: string;
+
+    @IsOptional()
+    @IsString()
     nombremadre?: string;
+
+    @IsOptional()
+    @IsString()
     grado?: string;
+
+    @IsOptional()
+    @IsString()
     seccion?: string;
+
+    @IsOptional()
+    @IsDateString()
     fechaingreso?: Date;
 }
