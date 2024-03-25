23bfd5055cccc4f1d767ee757eeaefd37b0ac592
MauGarr <3292719381105@ingenieria.usac.edu.gt>
2024-03-25 00:09:22 -0600

    Se mejora la interfaz de usuario al igual que la page de filtrar alumnos por grado

--- frontend/src/components/SearchByGrade.jsx
+++ frontend/src/components/SearchByGrade.jsx
@@ -13,37 +13,37 @@ const SearchByGrade = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="mt-10">
                 <input
-                    className= 'bg-gray-600 p-3 rounded-lg block w-auto mb-3' placeholder='Ingresa el grado'
+                    className= 'bg-gray-600 p-3 rounded-lg block w-auto mb-2' placeholder='Ingresa el grado'
                     type="text"
                     value={grade}
                     onChange={(e) => setGrade(e.target.value)}
                 />
                 <button className='bg-indigo-600 p-3 rounded-lg block text-white mr-4' type="submit">Buscar</button>
             </form>
-            <table className="table-auto">
-                <thead>
+            <table className="min-w-full divide-y divide-gray-200 mt-10">
+                <thead className="bg-gray-50">
                     <tr>
-                        <th className="px-4 py-2">Nombre</th>
-                        <th className="px-4 py-2">Fecha de Nacimiento</th>
-                        <th className="px-4 py-2">Nombre del Padre</th>
-                        <th className="px-4 py-2">Nombre de la Madre</th>
-                        <th className="px-4 py-2">Grado</th>
-                        <th className="px-4 py-2">Sección</th>
-                        <th className="px-4 py-2">Fecha de Ingreso</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Nombre</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Fecha de Nacimiento</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Nombre del Padre</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Nombre de la Madre</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Grado</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Sección</th>
+                        <th scope="col" className="px-1 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Fecha de Ingreso</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="bg-gray divide-y divide-gray-200">
                     {students.map((student) => (
-                        <tr key={student.id}>
-                            <td className="border px-4 py-2">{student.nombre}</td>
-                            <td className="border px-4 py-2">{student.fechanacimiento}</td>
-                            <td className="border px-4 py-2">{student.nombrepadre}</td>
-                            <td className="border px-4 py-2">{student.nombremadre}</td>
-                            <td className="border px-4 py-2">{student.grado}</td>
-                            <td className="border px-4 py-2">{student.seccion}</td>
-                            <td className="border px-4 py-2">{student.fechaingreso}</td>
+                        <tr key={student.id} className="hover:bg-sky-500 hover.cursor-pointer">
+                            <td className="px-1 py-3 whitespace-nowrap">{student.nombre}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.fechanacimiento}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.nombrepadre}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.nombremadre}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.grado}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.seccion}</td>
+                            <td className="px-1 py-3 whitespace-nowrap">{student.fechaingreso}</td>
                         </tr>
                     ))}
                 </tbody>
